c0259227c8cfdf66f7c3f58fae5a210d7d1343bd
francois papon <fpapon@yupik.com>
2024-03-09 19:06:53 +0100

    [minisite] Update theme

--- minisite-core/src/main/java/io/yupiik/tools/minisite/MiniSite.java
+++ minisite-core/src/main/java/io/yupiik/tools/minisite/MiniSite.java
@@ -655,7 +655,6 @@ public class MiniSite implements Runnable {
         if (configuration.isUseDefaultAssets()) {
             Stream.of(
                             "yupiik-tools-maven-plugin/minisite/assets/css/theme.css",
-                            "yupiik-tools-maven-plugin/minisite/assets/css/theme-old.css",
                             "yupiik-tools-maven-plugin/minisite/assets/js/minisite.js",
                             "yupiik-tools-maven-plugin/minisite/assets/images/logo.svg")
                     .forEach(resource -> {
